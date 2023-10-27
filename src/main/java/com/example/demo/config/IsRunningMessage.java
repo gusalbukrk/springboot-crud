178444eda6ef20294561396f358ec6ea16bb57a5
gusalbukrk <gusalbukrk@gmail.com>
2023-10-27 15:01:13 -0300

    conditionally run a piece of code depending on config property

--- src/main/java/com/example/demo/config/IsRunningMessage.java
+++ src/main/java/com/example/demo/config/IsRunningMessage.java
@@ -1,8 +1,10 @@
 package com.example.demo.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!dev") // don't run when `spring.profiles.active` property is set to dev
 // can also be used to populate database during initialization
 @Component
 public class IsRunningMessage implements CommandLineRunner {
